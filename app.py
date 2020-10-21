875f1985bbd19d367f750486a2deadbe0665f504
babetteblanquet <65524644+babetteblanquet@users.noreply.github.com>
2020-10-21 16:12:31 +1100

    app update
    tweeter api

--- app.py
+++ app.py
@@ -1,58 +1,99 @@
 import streamlit as st
 import tweepy
-from wordcloud import WordCloud
+# from wordcloud import WordCloud
 import pandas as pd
 import numpy as np
 import re
 import matplotlib.pyplot as plt
+# Import API key
+from config import consumerKey
+from config import consumerSecret
+from config import accessToken
+from config import accessTokenSecret
+
+# Create the authentication object
+authenticate = tweepy.OAuthHandler(consumerKey, consumerSecret)
+
+# Set the access token and access token secret
+authenticate.set_access_token(accessToken, accessTokenSecret)
+
+# Creating the API object while passing in the auth information
+api = tweepy.API(authenticate, wait_on_rate_limit=True)
 
 
-
-#st.title("Streamlit example")
+# st.title("Streamlit example")
 html_temp = """
     <div style="background-color:#1da1f2;padding:10px">
     <h1 style="color:white;text-align:center;"> Twitter sentiment analysis</h1>
     </div>
     """
-st.markdown(html_temp,unsafe_allow_html=True)
+st.markdown(html_temp, unsafe_allow_html=True)
 
 st.title("Get the 100 latest tweets")
 st.subheader("""Get the 100 latest tweets""")
 st.write("Get the 100 latest tweets")
 tweet_handle = st.text_input("Enter tweet handle without the @")
 
-def cleanTxt(text):
-	text = re.sub('@[A-Za-z0–9]+', '', text) #Removing @mentions
-	text = re.sub('#', '', text) # Removing '#' hash tag
-	text = re.sub('RT[\s]+', '', text) # Removing RT
-	text = re.sub('https?:\/\/\S+', '', text) # Removing hyperlink
-	return text	
 
 def get_data(user_name):
 
-	tweets = api.user_timeline(screen_name=tweet_handle, count = 100, lang ="en", tweet_mode="extended")
-	df = pd.DataFrame([tweet.full_text for tweet in tweets], columns=['Tweets'])
+    posts = api.user_timeline(
+        screen_name=tweet_handle, count=100, lang="en", tweet_mode="extended")
+
+    df = pd.DataFrame(
+        [tweet.full_text for tweet in posts], columns=['Tweets'])
+
+    def cleanTxt(text):
+        text = re.sub('@[A-Za-z0–9]+', '', text)  # Removing @mentions
+        text = re.sub('#', '', text)  # Removing '#' hash tag
+        text = re.sub('RT[\s]+', '', text)  # Removing RT
+        text = re.sub('https?:\/\/\S+', '', text)  # Removing hyperlink
+        return text
 
     # Clean the tweets
-	df['Tweets'] = df['Tweets'].apply(cleanTxt)
-    return df
-           
+    df['Tweets'] = df['Tweets'].apply(cleanTxt)
 
-## Call the sentiment analysis algorythm
+    # def getSubjectivity(text):
+    # 	return TextBlob(text).sentiment.subjectivity
+
+    # 			# Create a function to get the polarity
+    # def getPolarity(text):
+    # 	return  TextBlob(text).sentiment.polarity
+
+    # 			# Create two new columns 'Subjectivity' & 'Polarity'
+    # df['Subjectivity'] = df['Tweets'].apply(getSubjectivity)
+    # df['Polarity'] = df['Tweets'].apply(getPolarity)
+
+    # def getAnalysis(score):
+    # 	if score < 0:
+    # 		return 'Negative'
+
+    # 	elif score == 0:
+    # 		return 'Neutral'
+
+    # 	else:
+    # 		return 'Positive'
+
+    # df['Analysis'] = df['Polarity'].apply(getAnalysis)
+    return df
+
+
+if st.button("Show Data"):
+
+    st.success("Fetching Last 100 Tweets")
+
+    df = get_data(tweet_handle)
+
+    st.write(df)
+
+
+# Call the sentiment analysis algorythm
 # Load the model
 # from tensorflow.keras.models import load_model
 # model = load_model("mnist_trained.h5")
 
 # def getSubjectivity(text):
 # 				return TextBlob(text).sentiment.subjectivity
-    
-
-#Button show data
-if st.button("Show Tweets"):
-
-			st.success("Fetching Last 100 Tweets")
-			df=get_data(tweet_handle)
-			st.write(df)
 
 
 # if __name__=='__main__':
